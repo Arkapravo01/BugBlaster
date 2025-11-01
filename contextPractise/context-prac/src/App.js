d951224c4c499f55ceedd93860c151b7e511c7e2
Arkapravo <arkapravochakrabarti188@gmail.com>
2025-11-01 20:46:28 +0530

    80-Done

--- contextPractise/context-prac/src/App.js
+++ contextPractise/context-prac/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import MessageContext from "./context/MessageContext";
+import { useContext } from "react";
 
 function App() {
+  const message = "Hello World";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MessageContext.Provider value={message}>
+      <ComponentA />
+    </MessageContext.Provider>
   );
 }
 
+function ComponentA() {
+  return (
+    <div>
+      <ComponentB />
+    </div>
+  );
+}
+function ComponentB() {
+  const message = useContext(MessageContext);
+  return <div>Message:{message}</div>;
+}
 export default App;
---
+++ contextPractise/context-prac/src/context/MessageContext.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { createContext } from "react";
+
+function MessageContext() {
+  const MessageContext = createContext();
+
+}
+
+export default MessageContext;
